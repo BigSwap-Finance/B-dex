174ade770d94571da3492460d039c93f90367092
BigSwap Finance <86725876+BigSwap-Finance@users.noreply.github.com>
2021-08-23 15:42:49 -0400

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -19,6 +19,7 @@ import {
     MARKET_APP_BASE_PATH,
 } from './common/constants';
 import { AppContainer } from './components/app';
+import { PageLoading } from './components/common/page_loading';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
 import { history, store } from './store';
