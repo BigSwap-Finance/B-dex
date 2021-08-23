d38ec02ba9a89df1939a73970850769a5f996f38
BigSwap Finance <86725876+BigSwap-Finance@users.noreply.github.com>
2021-08-23 12:20:17 -0400

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -78,6 +78,4 @@ ReactDOM.render(Web3WrappedApp, document.getElementById('root'));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-if (!envUtil.isMobileOperatingSystem()) {
-    serviceWorker.register();
-}
+serviceWorker.unregister();
