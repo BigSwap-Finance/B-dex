da1a293ca581653488ef8aaba4ed5411ce3299b4
BigSwap Finance <86725876+BigSwap-Finance@users.noreply.github.com>
2021-09-16 08:10:44 -0400

    Update serviceWorker.js

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -126,10 +126,23 @@ function checkValidServiceWorker(swUrl, config) {
     });
 }
 
-export function unregister() {
+export function register() {
   if ('serviceWorker' in navigator) {
     navigator.serviceWorker.ready.then(registration => {
-      registration.unregister();
+      registration.register();
     });
   }
+}
+
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('/sw.js').then(function(registration) {
+      // Registration was successful
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }, function(err) {
+      // registration failed :(
+      console.log('ServiceWorker registration failed: ', err);
+    });
+  });
 }
